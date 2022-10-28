71f5d825ae03964d30e2d5991e2d4b176f3af9b5
JenaroLeal <jenaroleal@hotmail.com>
2022-10-28 13:32:27 +0200

    primer commit

---
+++ prSeries/src/dao/SerieDao.java
@@ -0,0 +1,83 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import pojo.Serie;
+import util.DataBaseConnection;
+
+public class SerieDao implements Dao<Serie>{
+	
+	private static Connection connection;
+	
+	public SerieDao() {
+		
+	}
+
+	@Override
+	public ArrayList<Serie> buscatTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Serie buscarPorId(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertar(Serie serie) {
+	
+		connection=openConnection();
+		
+		String query = "insert into series (titulo, edad, plataforma) values (?, ?, ?)";
+		
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			ps.setString(1, serie.getTitulo());
+			ps.setInt(2, serie.getEdad());
+			ps.setString(3, serie.getPlataforma());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		closeConnection();
+		
+	}
+
+	@Override
+	public void modificar(Serie t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void borrar(Serie t) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private static Connection openConnection() {
+
+		DataBaseConnection dbConnection = new DataBaseConnection();
+		connection = dbConnection.getConnection();
+		return connection;
+	}
+
+	private static void closeConnection() {
+		try {
+			connection.close();
+			connection = null;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+}
